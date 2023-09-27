bda6f1d4f1c5c983187f07370c5b004ab360a783
Kelley Sheen <kelleysheen@gmail.com>
2023-09-26 17:01:15 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    
+    return (Math.abs(someValue - 42));
+  }
+
+function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    return (distanceFromHqInBlocks(someValue)* 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start-destination)*264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let result;
+    
+    if (distance <= 400) {
+        result = 0;
+    } else if (distance >400 && distance <=2000) {
+        result = 2.56;
+    } else if (distance >2000 && distance <=2500) {
+        result = 25;
+    } else if (distance >2500) {
+        return 'cannot travel that far';
+    }
+
+    return result;
+}
